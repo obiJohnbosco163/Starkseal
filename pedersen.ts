3f25bd6f21c818aa8cca7baa117e8d34d3694196
Obi <123234391+obiJohnbosco163@users.noreply.github.com>
2026-02-23 04:21:31 +0100

    Delete pedersen.ts
    Signed-off-by: Obi  <123234391+obiJohnbosco163@users.noreply.github.com>

--- pedersen.ts
+++
@@ -1,9 +0,0 @@
-import { pedersen } from "starknet";
-
-// Utility to hash bid and secret using Pedersen hash
-export function pedersenHash(bid: string, secret: string) {
-  // Convert bid and secret to BigInt (felt)
-  const bidFelt = BigInt(bid);
-  const secretFelt = BigInt("0x" + Buffer.from(secret, "utf8").toString("hex"));
-  return pedersen(bidFelt, secretFelt).toString();
-}
