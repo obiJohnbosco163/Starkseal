2de3c354667908a9b296d52802761eaf2f941932
Obi <123234391+obiJohnbosco163@users.noreply.github.com>
2026-02-23 04:35:16 +0100

    Add files via upload
    adder
    
    Signed-off-by: Obi  <123234391+obiJohnbosco163@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+<<<<<<< HEAD
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0df233",
+        "background-light": "#f5f8f6",
+        "background-dark": "#102213",
+      },
+      fontFamily: {
+        display: ["Inter", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
+=======
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0df233",
+        "background-light": "#f5f8f6",
+        "background-dark": "#102213",
+      },
+      fontFamily: {
+        display: ["Inter", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
+>>>>>>> 807626699935d12e60653d05b288d191251069a3
