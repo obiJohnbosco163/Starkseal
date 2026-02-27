845639cbbf081bf6c6eecbe709002b760d23c86e
gem66888-ui <gem66888@gmail.com>
2026-02-27 01:05:36 +0100

    added react

--- frontend/starkseal_frondend/src/components/layout/nav.jsx
+++ frontend/starkseal_frondend/src/components/layout/nav.jsx
@@ -1,4 +1,6 @@
-export default function Nav() {
+import { Link } from "react-router-dom";
+import { Element, Link as ScLink } from "react-scroll";
+export default function Navbar({ get_balance }) {
   return (
     <div>
       {/* Navigation */}
@@ -7,32 +9,42 @@ export default function Nav() {
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="flex justify-between items-center h-16">
             <div class="flex items-center gap-3">
-              <div class="flex items-center justify-center size-10 bg-primary/10 rounded-lg">
-                <img
-                  src="img_20260216_203641.png.png"
-                  alt="StarkSeal"
-                  style="width: 100%; height: 100%; border-radius: 50%;"
-                />
-              </div>
+              <Link to="./">
+                <div class="flex items-center justify-center size-10  rounded-full">
+                  <img
+                    src="/images/img_20260216_203641.png.png"
+                    alt="StarkSeal"
+                    className="w-full h-full rounded-full"
+                  />
+                </div>
+              </Link>
+
               <span class="text-xl font-bold tracking-tight text-slate-900 dark:text-white uppercase">
                 StarkSeal
               </span>
             </div>
             <div class="hidden md:flex items-center gap-8">
-              <a
+              <ScLink
+                to="active_auction"
+                smooth={true}
+                duration={500}
+                offset={-100}
                 class="text-sm font-medium hover:text-primary transition-colors"
-                href="#"
               >
                 Explore
-              </a>
-              <a
+              </ScLink>
+
+              <Link
+                to="/dashboard"
                 class="text-sm font-medium hover:text-primary transition-colors"
-                href="dashboardpage.html"
               >
                 My Bids
-              </a>
+              </Link>
               <div class="h-4 w-px bg-primary/20"></div>
-              <button class="bg-primary hover:bg-primary/90 text-background-dark px-5 py-2 rounded-lg text-sm font-bold transition-all shadow-lg shadow-primary/20 flex items-center gap-2">
+              <button
+                onClick={get_balance}
+                class="bg-primary hover:bg-primary/90 text-background-dark px-5 py-2 rounded-lg text-sm font-bold transition-all shadow-lg shadow-primary/20 flex items-center gap-2"
+              >
                 <span class="material-symbols-outlined text-sm">
                   account_balance_wallet
                 </span>
